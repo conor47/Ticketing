a1e268d36ba481217ccd4b5c7fdcefed2483f1bf
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-09-05 18:16:48 +0100

    Developed currentuser route for checking the auth credentials of a user. Route will be used by the client to check if a given user is authenticated

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+
+// route for handling client requests to check if a user is authenticated
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there');
+  // below conditional is equivalent to if(!req.session || !req.session.jwt)
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (error) {
+    res.send({ currentuser: null });
+  }
 });
 
 export { router as currentUserRouter };
