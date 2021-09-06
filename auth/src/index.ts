affa813dc9863d2fef12f49dc4a8344c52f8def7
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-09-06 04:00:49 +0100

    Seperated index.ts file into app.ts and index.ts. This is to aid in testing with Supertest. We can use our configured express server now in two locations

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,39 +1,7 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import cookieSession from 'cookie-session';
+// The purpose of this file is to start our express server and form necessary connections eg to Database
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
 import mongoose from 'mongoose';
-
-const app = express();
-// traffic is being proxied to our application through ingress-nginx. Express by default will not trust this proxy so we must explicitly
-// tell it to
-app.set('trust proxy', true);
-app.use(json());
-// bringing cookie session middleware into use
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   // check to ensure that the necessary environment variables have been set. We perform this check here instead of eg in a route , as
