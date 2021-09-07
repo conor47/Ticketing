be5c135c729ad0e930eb9e9136dce5c915102e2a
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-09-07 07:15:02 +0100

    Installed axios and hooked up signup page to auth service

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const response = await axios.post('/api/users/signup', {
+      email,
+      password,
+    });
+    console.log(response.data);
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign up</h1>
       <div className="form-group">
         <label>Email address</label>
-        <input type="text" className="form-control" />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="form-group">
         <label>Password</label>
-        <input type="passowrd" className="form-control" />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="passowrd"
+          className="form-control"
+        />
       </div>
       <button className="btn-primary">Sign up</button>
     </form>
