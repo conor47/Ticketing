c21b3da711cba3dbadbd80427fab2e0ad4f88f55
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-09-11 18:19:07 +0100

    type checking on message data

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -7,9 +7,13 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log(`event data !`, data);
     // we manually acknowledge the event, telling nats not to re-emit the event anymore
+    console.log(data.id);
+    console.log(data.title);
+    console.log(data.price);
+
     msg.ack();
   }
 }
