cbe32d2000ef0d2307ad1d34f2d16b263afb33c8
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-09-11 19:54:08 +0100

    Publisher publish method now returns a promise. Allows for use of async await on event publishing

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -11,15 +11,19 @@ const stan = nats.connect('ticketing', 'abc', {
 
 // when the client successfully connects to the nat streaming server a connect event is emitted. We listen for this and then perform some action
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to nats');
 
   const publisher = new TicketCreatedPublisher(stan);
-  publisher.publish({
-    id: '123',
-    title: 'concert',
-    price: 20,
-  });
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'concert',
+      price: 20,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // Publish takes three arguements. First is the channel name to which the event is posted. Second is the data/message being posted.
