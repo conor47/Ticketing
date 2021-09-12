2dcac5a5e4cef1b90127f68762372d75a4dadc77
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-09-12 05:05:36 +0100

    Added routing rule to ingress controller for order service. Added basic routes to orders service and wired them up to express app

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -6,10 +6,11 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
 import { NotFoundError, errorHandler, currentUser } from '@clmicrotix/common';
-import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes';
-import { updateTicketRouter } from './routes/update';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes';
+
 const app = express();
 // traffic is being proxied to our application through ingress-nginx. Express by default will not trust this proxy so we must explicitly
 // tell it to
@@ -26,10 +27,10 @@ app.use(
 );
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
