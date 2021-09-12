ecc245962c6deb3a9e7d1791eac90867d62a0a38
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-09-12 22:46:48 +0100

    Added request validation and authentication to new order route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@clmicrotix/common';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      // we are checking that the user provides a valid mongo Id. Note this introduces some subtle coupling between the orders and tickets services
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must not be empty'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
