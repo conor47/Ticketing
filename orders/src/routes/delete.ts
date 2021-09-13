814b9d9d0653ccb738e3ee4b6ab32d0627a33f05
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-09-13 04:54:49 +0100

    Development of show and delete routes. Show route tests

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,33 @@
 import express, { Request, Response } from 'express';
+import { Order, OrderStatus } from '../models/order';
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from '@clmicrotix/common';
 
 const router = express.Router();
 
-router.post('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.id !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+
+    res.send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
