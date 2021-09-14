0605ef8dac0636de4a5d4d7610a885ff84004c6c
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-09-14 23:39:29 +0100

    Updated tickets tocket model. Completed tickets order created listener

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,6 +1,8 @@
 import { Listener, OrderCreatedEvent, Subjects } from '@clmicrotix/common';
 import { Message } from 'node-nats-streaming';
 
+import { Ticket } from '../../../models/ticket';
+
 import { queueGroupName } from './queue-group-name';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
@@ -8,5 +10,20 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    //   find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+    // if no ticket throw error
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+    // mark the ticket as reserved by setting orderid property
+    ticket.set({ orderId: data.id });
+
+    // save the ticket
+    await ticket.save();
+
+    // acknowledge the message
+    msg.ack();
+  }
 }
