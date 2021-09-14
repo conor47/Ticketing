5cc9a618f8491dd5e9c43940c10ca977ca69e60d
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-09-14 02:15:55 +0100

    Added more testing for ticket model versioning

--- tickets/models/__test__/ticket.test.ts
+++ tickets/models/__test__/ticket.test.ts
@@ -1,5 +1,51 @@
-import {Ticket} from '../ticket'
+import { Ticket } from '../ticket';
 
-if('implemented OCC', async () => {
-    
-})
+it('implemented OCC', async () => {
+  // create an instance of a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 10,
+    userId: '1010',
+  });
+
+  // save the ticket
+  await ticket.save();
+
+  // fetch the ticket twice
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  // make two seperate changes to to tickets
+  firstInstance!.set({ price: 50 });
+  secondInstance!.set({ price: 60 });
+
+  // save the first changed ticket
+  await firstInstance!.save();
+
+  // save the second changed ticket and expect and error
+  try {
+    await secondInstance!.save();
+  } catch (error) {
+    return error;
+  }
+
+  throw new Error('Should be unreachable');
+});
+
+it('increments versin number each save', async () => {
+  // create an instance of a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 10,
+    userId: '123',
+  });
+
+  // save the ticket
+  await ticket.save();
+
+  expect(ticket.version).toEqual(0);
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
+});
