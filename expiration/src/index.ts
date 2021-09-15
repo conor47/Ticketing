945bf1a4d0be161b13bbe611f25c75dc8ce12d55
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-09-15 04:37:41 +0100

    Additional setup for expiration service. Installed bull package.

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,22 +1,9 @@
-// The purpose of this file is to start our express server and form necessary connections eg to Database
-
-import mongoose from 'mongoose';
-
-import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
-import { OrderCreatedListener } from './events/listeners/order-created-listener';
-import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
   // check to ensure that the necessary environment variables have been set. We perform this check here instead of eg in a route , as
   // these secrets are crtical to the functioning of our application
 
-  if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY key must be defined');
-  }
-  if (!process.env.MONGO_URI) {
-    throw new Error('Mongo URI must be defined');
-  }
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error('NATS_CLIENT_ID must be defined');
   }
@@ -40,19 +27,9 @@ const start = async () => {
     });
     process.on('SIGTERM', () => natsWrapper.client.close());
     process.on('SIGINT', () => natsWrapper.client.close());
-
-    new OrderCreatedListener(natsWrapper.client).listen();
-    new OrderCancelledListener(natsWrapper.client).listen();
-
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('connecting to DB');
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.log(err);
   }
-
-  app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!!!!!');
-  });
 };
 
 start();
