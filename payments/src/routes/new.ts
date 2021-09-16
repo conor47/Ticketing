c6c93e8ef1be4d7fc861320b8aad377ee5fae3f9
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-09-16 06:12:11 +0100

    Extended payments new route logic

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -5,9 +5,12 @@ import {
   validateRequest,
   BadRequestError,
   NotFoundError,
+  NotAuthorizedError,
+  OrderStatus,
 } from '@clmicrotix/common';
 
 import { Order } from '../models/order';
+import { idText } from 'typescript';
 
 const router = express.Router();
 
@@ -17,9 +20,28 @@ router.post(
   [body('token').not().isEmpty(), body('orderId').not().isEmpty()],
   validateRequest,
   async (req: Request, res: Response) => {
-    res.send({ success: true });
+    // extract token and order id from request body
+    const { token, orderId } = req.body;
+
+    // retrieve order based on orderId
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    // check that the requesting user has the same id as the orders userId
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    // check that the order is not already cancelled
+    if ((order.status = OrderStatus.Cancelled)) {
+      throw new BadRequestError('Order is expired. Payment not allowed.');
+    }
+
+    res.send({ succes: true });
   }
 );
 
 export { router as createChargeRouter };
-,
